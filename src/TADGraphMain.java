4d03472bf8a15bdb1e4f5091c2d3378dee3bad63
patricia.macedo <patricia.macedo@estsetubal.ips.pt>
2021-10-29 15:57:28 +0100

    Changes in GraphUtis Add Model package Delete releases

---
+++ src/TADGraphMain.java
@@ -0,0 +1,16 @@
+import pt.pa.adts.graph.Graph;
+import pt.pa.adts.graph.GraphEdgeList;
+import pt.pa.model.Bridge;
+import pt.pa.model.Local;
+
+public class TADGraphMain {
+    public static void main(String[ ] args){
+
+        Graph<Local, Bridge> graph = new GraphEdgeList<>();
+
+
+    }
+
+
+
+}
---
+++ src/pt/pa/model/Bridge.java
@@ -0,0 +1,27 @@
+package pt.pa.model;
+
+public class Bridge {
+    private String name;
+    private int cost;
+
+    public Bridge(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Bridge{" +
+                "name='" + name + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
